606a32ab01230f02cdacd4059ace5543613c68a6
jonathanrichardl <47639970+jonathanrichardl@users.noreply.github.com>
2021-07-30 01:15:23 -0700

    Update accelerationTest.py

--- Examples/accelerationTest.py
+++ Examples/accelerationTest.py
@@ -8,9 +8,9 @@ i2cid = 0
 acc = GY85(scl = sclPin, sda = sdaPin, i2cid = i2cid) # default mode / accelerometer only, magnetometer and gyroscope configuration skipped
 try:
     while True:
-        buffer = acc.readAcc()
+        buffer = acc.read_acc()
         print("Acceleration Value x = " + str(buffer[0])+", y = " + str(buffer[1])+", z = " + str(buffer[2]))
-        buffer = acc.calculateRP()
+        buffer = acc.calculate_rp()
         print("Roll Value = " + str(buffer[0])+", Pitch Value = " + str(buffer[1]))
         utime.sleep(1)
 except KeyboardInterrupt:
